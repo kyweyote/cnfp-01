7435761d7eb5156522aedb71743974cdd47e68b6
Kywe Yote <168730977+kyweyote@users.noreply.github.com>
2024-09-14 12:21:05 +0900

    modified versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,1 +1,12 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.32.0"
+    }
+  }
+}
 
+provider "kubernetes" {
+  # Configuration options
+}
